3c6b5e74814ca8bbca2344d451a48efb753e0acb
Dave Tang <me@davetang.org>
2014-05-03 10:44:50 +0900

    Script to download files

---
+++ download.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#download tRNAscan-SE
+wget http://lowelab.ucsc.edu/software/tRNAscan-SE-1.3.1.tar.gz
+
+#download bosTau7
+wget http://hgdownload-test.cse.ucsc.edu/goldenPath/bosTau7/bigZips/bosTau7.fa.gz
